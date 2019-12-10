8d477b4aa7e05222a28245cbbd2a4f13952a33d4
Sharon Watkins <swatkins876@gmail.com>
2019-12-10 15:50:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,23 +5,21 @@ module FormsLab
 
     # code other routes/actions here
     get '/' do
-          erb :root
-        end
+      erb :root
+    end
 
 
-        get '/new' do
-          erb :"pirates/new"
-        end
+    get '/new' do
+      erb :"pirates/new"
+    end
 
-        post '/pirates' do
-          @pirate = Pirate.new(params[:pirate])
-          params[:pirate][:ships].each do |details|
-            Ship.new(details)
-          end
-          @ships = Ship.all
-
-          erb :"pirates/show"
-        end
-
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
       end
+        @ships = Ship.all
+        erb :"pirates/show"
+      end
+end
 end
